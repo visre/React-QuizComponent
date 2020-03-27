2cf9b84a035d901c1ed4e99b85fbf552b06e3cef
visre <virac.sreng@sage.com>
2020-03-27 15:04:39 +0100

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -6,10 +6,10 @@ let quizData = require('./quiz_data.json');
 
 class Quiz extends Component {
     render() {
+        const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
         return (
             <div>
-                <QuizEnd show={quiz_position-1 === quizData.quiz_questions.length}/>
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+                {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
             </div>
         )
     }
